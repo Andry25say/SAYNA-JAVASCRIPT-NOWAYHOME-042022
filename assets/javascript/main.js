1b1f4d51c10c809126ad0dc35d395ff8afb05d0c
Andry <andryrichard2022@gmail.com>
2022-05-03 14:40:31 +0300

    Second commit amelioration codes

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,33 +1,42 @@
-let boutons1 = document.querySelector(".button1");
-boutons1.addEventListener("mouseover",effetHover1);
-function effetHover1(){
 
-    let css = boutons1.style;
-    css.backgroundColor = "#b11313";
-    css.color = "#ffffff";
-    css.fontWeight = "bold";
-    css.boxShadow = "5px 0px 25px #ffffff";
-}
-boutons1.addEventListener("mouseout",effetHover2);
-function effetHover2(){
-    boutons1.style.backgroundColor = "#ffffff";
-    boutons1.style.color = "#b11313";
-    css.fontWeight = "medium";
-}
-
-let boutons2 = document.querySelector(".button2");
-boutons2.addEventListener("mouseover",effetHover3);
-function effetHover3(){
-    boutons2.style.backgroundColor = "#b11313";
-    boutons2.style.color = "#ffffff";
-    boutons2.style.boxShadow = "5px 0px 25px #ffffff";
-}
-boutons2.addEventListener("mouseout",effetHover4);
-function effetHover4(){
-    boutons2.style.backgroundColor = "transparent";
-    boutons2.style.color = "#b11313";
-}
+/*effet hover des boutons1*/
 $(document).ready(function(){
+    $(".button1").hover(function(){
+        $(this).css({
+            "backgroundColor" : "#b11313",
+            "color" :"#ffffff",
+            "boxShadow":"0 0 25px #ffffff"
+        })
+    },
+    function(){
+        $(this).css({
+            "backgroundColor" : "#ffffff",
+            "color" :"#b11313",
+            "boxShadow":"none"
+
+        })
+    }
+    )
+/*effet hover des boutons2*/
+$(".button2").hover(function(){
+    $(this).css({
+        "backgroundColor" : "#b11313",
+        "color" :"#ffffff",
+        "boxShadow":"0 0 25px #ffffff"
+    })
+},
+function(){
+    $(this).css({
+        "backgroundColor" : "transparent",
+        "color" :"#b11313",
+        "boxShadow":"none"
+
+    })
+}
+)
+
+
+
     $(".media img").mouseover(function(){
         $(this).css("backgroundColor","#ffffff");
     })
@@ -36,13 +45,16 @@ $(document).ready(function(){
     })
     // ....................///////.....................
     $(".menu a").mouseover(function(){
-        $(this).css("borderBottom","1px solid #ffffff");
+        $(this).css({
+            "borderBottom":"1px solid #ffffff",
+
+        });
+        
     })
     $(".menu a").mouseout(function(){
         $(this).css("borderBottom","none");
     })
       // ....................///////.....................
-    $("h1").fadeIn()
 })
 
   
