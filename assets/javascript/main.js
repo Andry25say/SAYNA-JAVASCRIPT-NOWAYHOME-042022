19352b6a805b6282c550076dac92384134c2d980
Andry <andryrichard2022@gmail.com>
2022-05-04 16:52:47 +0300

    Cinquieme commit

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -64,24 +64,45 @@ $("h1").hide().animate({
  },"slow").show(2500);
 /*l'effet */
 $(window).scroll(function(){
+       
         $(".flou").css({
-            opacity:"40%",
+            opacity:"70%",
             float:"right",
             top:"0",
             position:"fixed",
-            marginLeft:"600px"
+            marginLeft:"600px",
         })
+    $(".flou2").css({
+        opacity:"50%",
+        float:"left",
+        top:"0",
+        position:"fixed",
+        marginLeft:"10px",
+        zIndex:"3"
+    })
+              
 })
-
+/*marvel */
+$(".marvel-text").css({
+    position:"relative",
+    left:"-70%",
+    zIndex:"1",
+    top:"0",
+    marginTop:"-300px"
+}).fadeIn().animate({
+    left:"200px"
+},4000)
+let o = window.scrollY();
+console.log(o);
 
 /*effet hover card unique*/
  $(".card-unique").mouseover(function(){
-    $(".card-text").css({
+    $(".card-body .card-text").css({
      display:"none"
      })
  },
  $(".card-unique").mouseout(function(){ 
-     $(".card-text").css({
+     $(".card-body .card-text").css({
      display:"inline-block"
  })
    
